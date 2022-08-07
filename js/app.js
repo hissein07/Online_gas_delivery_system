7915a38b1ca9c96b0acd66c0ec7568c7c43d753a
Hissein Ibrahim <hisseinibrahimb90@gmail.com>
2022-08-07 23:50:14 +0200

    responsive

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+//product page
+let previewContaine = document.querySelector('.produc-preview');
+let previewBox = previewContaine.querySelectorAll('.preview');
+
+document.querySelectorAll('.produc-containe .produc').forEach(produc =>{
+   produc.onclick = () =>{
+    previewContaine.style.display = 'flex';
+      let name = produc.getAttribute('data-name');
+      previewBox.forEach(preview =>{
+       let target = preview.getAttribute('data-target');
+        if(name == target){
+            preview.classList.add('activ');
+        }
+      });
+   };
+});
+
+previewBox.forEach(close =>{
+     close.querySelector('.fa-times').onclick = () =>{
+      close.classList.remove('activ');
+      previewContaine.style.display = 'none';
+     };
+});
+
+
+//  responsive 
+
+$(document).ready(function(){
+  $('.nav-trigger').click(function(){
+     $('.site-content-wrapper').toggleClass('scaled');
+  });  
+
+});
+
